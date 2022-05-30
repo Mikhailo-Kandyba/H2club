a4755a1094e757031c61fa58ee94abc5773b21f5
Mikhailo <mikhailo.kandyba0@gmail.com>
2022-05-30 22:47:08 +0300

    header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,22 @@ import { CardComponent } from './card/card.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { HeaderComponent } from './header/header.component';
+import {MatButtonModule} from '@angular/material/button';
+import { ButtonsHeaderComponent } from './buttons-header/buttons-header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    HeaderComponent
+    HeaderComponent,
+    ButtonsHeaderComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/buttons-header/buttons-header.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buttons-header',
+  templateUrl: './buttons-header.component.html',
+  styleUrls: ['./buttons-header.component.scss']
+})
+export class ButtonsHeaderComponent {
+  searchIcon = './assets/images/search.svg'
+}
